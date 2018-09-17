bb790de1c1361e8f6071422679027d43ccd649db
Leonardo Brondani Schenkel <leonardo@schenkel.net>
2018-09-17 20:19:02 +0200

    Initial implementation

---
+++ src/ofxstatement/plugins/al_bank.py
@@ -0,0 +1,88 @@
+import csv
+
+from ofxstatement.parser import CsvStatementParser
+from ofxstatement.plugin import Plugin
+from ofxstatement.statement import generate_transaction_id
+
+
+class ALBankPlugin(Plugin):
+    """Arbejdernes Landsbank <https://www.al-bank.dk>"""
+
+    def get_parser(self, filename):
+        return ALBankParser(open(filename, mode='r', encoding='ISO-8859-15'))
+
+
+class ALBankParser(CsvStatementParser):
+    date_format = '%d-%m-%Y'
+
+    def __init__(self, fin):
+        super().__init__(fin)
+        self.statement.bank_id = 'AL-Bank'
+        self.statement.currency = 'DKK'
+        self.headers = {}
+
+    def split_records(self):
+        reader = csv.reader(self.fin, delimiter=';')
+
+        headers = next(reader)
+        for i, header in enumerate(headers):
+            self.headers[header] = i
+            if header == 'Dato':
+                self.mappings['date'] = i
+            elif header == 'Valørdato':
+                self.mappings['date_user'] = i
+            elif header == 'Tekst':
+                self.mappings['memo'] = i
+            elif header == 'Beløb':
+                self.mappings['amount'] = i
+
+        return reader
+
+    def parse_record(self, line):
+        r = super().parse_record(line)
+
+        r.id = generate_transaction_id(r)
+        r.trntype = self.get_type(r)
+        if r.date_user:
+            r.date_user = self.parse_datetime(r.date_user)
+        r.memo = self.concat(r.memo, line, 'Tekst til modtager')
+        r.memo = self.concat(r.memo, line, 'Supp. tekst til modtager')
+        if r.amount < 0:
+            r.memo = self.concat(r.memo, line, 'Modtagernavn')
+            r.memo = self.concat(r.memo, line, 'Modtagerkonto')
+        elif r.amount > 0:
+            r.memo = self.concat(r.memo, line, 'Indbetaler')
+
+        return r
+
+    def parse_float(self, value):
+        value = value.replace(',', '.')
+        return super().parse_float(value)
+
+    def concat(self, text, line, header):
+        result = text or ''
+        value = self.get_value(line, header)
+        if value:
+            components = result.split(' | ')
+            if value not in components:
+                if result:
+                    result += ' | '
+                result += value
+        return result
+
+    def get_value(self, line, header):
+        if header in self.headers:
+            index = self.headers[header]
+            value = line[index]
+            if value:
+                return value.strip()
+        return ''
+
+    @staticmethod
+    def get_type(line):
+        if line.amount > 0:
+            return 'CREDIT'
+        elif line.amount < 0:
+            return 'DEBIT'
+        else:
+            return 'OTHER'
